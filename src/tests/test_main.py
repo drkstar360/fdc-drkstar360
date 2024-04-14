364c576588e36bda19d3ef5d1c3afad3caf374da
drkstar360 <avenger.add@gmail.com>
2024-04-14 11:14:57 +0600

    initial

---
+++ src/tests/test_main.py
@@ -0,0 +1,6 @@
+import unittest
+from src.fig_data_challenge.models import Product, Restaurant, Category
+
+class Testmain(unittest.TestCase):
+    def test_main(self):
+        print("this is the testcase")
